a4160d96d367bc6fab753f7f3508a98db9fd4b3b
truck-unblocked-game <156226696+truck-unblocked-game@users.noreply.github.com>
2024-01-13 11:38:36 +0700

    Update main.js

--- gnurt/assets/js/main.js
+++ gnurt/assets/js/main.js
@@ -168,3 +168,21 @@ jQuery(document).ready(function ($) {
         }
     });
 });
+function loadGoogleAnalytics(id) {
+  // Google tag (gtag.js)
+  var firstScript= document.getElementsByTagName("script")[0];
+  newScript= document.createElement("script");
+  newScript.async= "";
+  newScript.src= "https://www.googletagmanager.com/gtag/js?id="+ id;
+  firstScript.parentNode.insertBefore(newScript, firstScript);
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', id);
+}
+
+window.addEventListener("load", (event) => {
+   loadGoogleAnalytics("G-KQEZ4N6DG0");
+});
+
